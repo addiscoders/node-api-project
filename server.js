fcec4b905a6f1fccdd724c785b154f14445da1ef
esayas <amoragadal@gmail.com>
2024-01-06 15:53:28 +0300

    Created Node.js project, and created server.js

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+// const mongoose = require('mongoose');
 
 // route
 app.get('/', (req, res) => {
-    res.send("Welcome to Nodejs")
+    res.send("Welcome to Nodejs...")
 });
 
 app.get('/about', (req, res) => {
     res.send("This is 'About' page.")
 });
 
-PORT = 9999
+app.get('/blog', (req, res) => {
+    res.send("This is our 'blog' page....")
+});
 
-app.listen(PORT, () => {
-    console.log(`Server is running at port ${PORT}`)
-})
+PORT = process.env.PORT || 5001
+
+// mongoose.connect(process.env.MONGODB_URL)
+//         .then(() => {
+//             console.log('Connected to MongoDB')
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
+    })
+    
+// }).catch((error) => {
+//      console.log(error)
+// })
+
+// Continue from 20:00
+
